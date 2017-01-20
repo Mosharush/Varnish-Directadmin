d6a644a2a4d9538e42b628dcb4c0bdcfdca542c9
csabyka <csabyka@users.noreply.github.com>
2017-01-21 00:05:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,20 +26,18 @@ else
 fi
 
 #Removing Previous Varnish Cache
-yum -y remove varnish
+apt-get remove varnish -y
 
-#Install Packages & Libraries
-rpm -ivh https://dl.fedoraproject.org/pub/epel/6/x86_64/jemalloc-3.6.0-1.el6.x86_64.rpm
-rpm -ivh https://repo.varnish-cache.org/redhat/varnish-4.0.el6.rpm
-yum -y install lynx
+#Install Packages & Libraries & Varnish Cache
+apt-get install apt-transport-https -y
+curl https://repo.varnish-cache.org/GPG-key.txt | sudo apt-key add -
+echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" \
+    | sudo tee -a /etc/apt/sources.list.d/varnish-cache.list
+apt-get update
+apt-get install varnish lynx -y
 
-#Install Varnish Cache
-yum -y install varnish
+#configure httpd
 
-#Config Varnish Cache
-cd /etc/varnish/
-ip=$(( lynx --dump cpanel.net/showip.cgi ) 2>&1 | sed "s/ //g")
-sed -i "s#host = \"127.0.0.1\"#host = \"$ip\"#g" ./default.vcl
 cd /etc/httpd/conf/
 sed -i 's#Listen 80#Listen 8080#g' ./httpd.conf
 cd extra
@@ -49,11 +47,12 @@ cp -p /usr/local/directadmin/data/templates/virtual_host2_sub.conf  /usr/local/d
 cp -p  /usr/local/directadmin/data/templates/redirect_virtual_host.conf  /usr/local/directadmin/data/templates/custom/redirect_virtual_host.conf
 cd /usr/local/directadmin/data/templates/custom
 sed -i 's/<VirtualHost |IP|:|PORT_80| |MULTI_IP|>/<VirtualHost 127.0.0.1:8080 |IP|:8080 |MULTI_IP|>/g' *
-cd
-wget http://www.stderr.net/apache/rpaf/download/mod_rpaf-0.6.tar.gz
-tar xzf mod_rpaf-0.6.tar.gz
-cd mod_rpaf-0.6
-apxs -cia mod_rpaf-2.0.c
+
+cd /usr/src
+wget https://github.com/gnif/mod_rpaf/archive/stable.zip
+unzip stable.zip
+cd mod_rpaf-stable/
+apxs -cia mod_rpaf.c
 cd
 ip=$(( lynx --dump cpanel.net/showip.cgi ) 2>&1 | sed "s/ //g")
 echo "<IfModule mod_rpaf-2.0.c>
@@ -61,17 +60,26 @@ RPAFenable On
 RPAFsethostname On
 RPAFproxy_ips 127.0.0.1 $ip
 RPAFheader X-Forwarded-For
-</IfModule>" >> /etc/httpd/conf/extra/httpd-includes.conf
+</IfModule>" >> /etc/httpd/conf/extra/httpd-rpaf.conf
+echo "Include /etc/httpd/conf/extra/httpd-rpaf.conf" >> /etc/httpd/conf/extra/httpd-includes.conf
 echo "action=rewrite&value=httpd" >> /usr/local/directadmin/data/task.queue
-cd /etc/sysconfig/
+
+#Config Varnish Cache
+cd /etc/default/
+ip=$(( lynx --dump cpanel.net/showip.cgi ) 2>&1 | sed "s/ //g")
+sed -i "s#host = \"127.0.0.1\"#host = \"$ip\"#g" ./varnish
+
+#listen @80
+cd /etc/default/
 sed -i 's#VARNISH_LISTEN_PORT=6081#VARNISH_LISTEN_PORT=80#g' ./varnish
+
+#Start When Reboot
+sed -i 's#START=no#START=yes#g' ./varnish
 
 #Start Varnish Cache
 service httpd restart
 service varnish start
 
-#Start When Reboot
-chkconfig varnish on
 
 echo -e "$GREEN---------------------------------------------------$RESET"
 echo -e "$GREEN      Varnish Cache Install Completed       $RESET"
